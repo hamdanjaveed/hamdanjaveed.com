221e9d5fd58cb68afd074b20039f012db27401ed
hamdanjaveed <hamdan@hamdanjaveed.com>
2013-08-25 17:58:41 -0400

    Added a navigation bar with a mouseover color animation

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
+    /* the parallax effect for the header */
     $(window).scroll(function() {
-        var s = $(window).scrollTop();
-        $("header").css("top", "-" + (s/5) + "px");
+        var currentScrollValue = $(window).scrollTop();
+        $("header").css("top", "-" + (currentScrollValue / 5) + "px");
+        if (currentScrollValue > 500 && $("#navbar").css("opacity") == 0) {
+            $("#navbar").animate({opacity:1}, 500);
+        } else if (currentScrollValue < 500 && $("#navbar").css("opacity") == 1) {
+            $("#navbar").animate({opacity:0}, 500);
+        }
     });
-    console.log("hi");
+    
+    /* the mouse over background animation effect for the links in the navigation bar */
+    $("#navbar h2").mouseenter(function() {
+        $(this).animate({backgroundColor:"rgba(214, 99, 96, 0.5)"}, 100);
+    });
+    $("#navbar h2").mouseleave(function() {
+        $(this).animate({backgroundColor:"rgba(214, 99, 96, 0)"}, 100);
+    });
 });

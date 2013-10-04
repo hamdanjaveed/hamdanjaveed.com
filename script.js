96b7b28f9fa5f4ea3dadabd1f89c9e4dac7d967d
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-10-04 01:27:13 -0400

    Created a simple website design with one fading element

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
+var fadeOutTimer = null;
+
 $(document).ready(function() {
-    /* the parallax effect for the header */
-    $(window).scroll(function() {
-        var currentScrollValue = $(window).scrollTop();
-        $("header").css("top", "-" + (currentScrollValue / 5) + "px");
-        if (currentScrollValue > 500 && $("#navbar").css("opacity") == 0) {
-            $("#navbar").animate({opacity:1}, 500);
-        } else if (currentScrollValue < 500 && $("#navbar").css("opacity") == 1) {
-            $("#navbar").animate({opacity:0}, 500);
+    $("html").mousemove(function() {
+        $(".fadein").stop().animate({opacity: "0.5"}, 500);
+        if (fadeOutTimer != null) {
+            clearTimeout(fadeOutTimer);
         }
+        fadeOutTimer = setTimeout(hide_elements, 1500);
+        console.log(fadeOutTimer);
     });
     
-    /* the mouse over background animation effect for the links in the navigation bar */
-    $("#navbar h2").mouseenter(function() {
-        $(this).animate({backgroundColor:"rgba(214, 99, 96, 0.5)"}, 100);
-    });
-    $("#navbar h2").mouseleave(function() {
-        $(this).animate({backgroundColor:"rgba(214, 99, 96, 0)"}, 100);
-    });
+    
 });
+    
+function hide_elements() {
+    $(".fadein").animate({opacity: "0"}, 500);
+}

50dd448c41d957fb9115347b335f00c13c78c524
Hamdan Javeed <hamdan@hamdanjaveed.com>
2014-07-22 15:06:08 -0400

    Converting project over to SASS, re-added github feed and footer

---
+++ script.js
@@ -0,0 +1,18 @@
+$.ajax({
+    type: "GET",
+    url: "https://api.github.com/users/hamdanjaveed/repos?sort=updated",
+    dataType: "json",
+    success: function(repos) {
+        for (var i = 0; i < 6; i++) {
+            var project =
+                '<a href="' + repos[i].html_url + '">\
+                    <div class="project" style="background-color:' + repos[i].language + ';">\
+                    <br \><br \><br \>\
+                        <h1>' + repos[i].name.charAt(0).toUpperCase() + '</h1>\
+                    </div>\
+                </a>';
+
+            $("#currentWork").append(project);
+        }
+    }
+});

59309d232f44d10f0b55c0572f12a688e8354b94
Hamdan Javeed <hamdan@hamdanjaveed.com>
2014-07-26 14:00:09 -0400

    Site fully converted to SASS

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $.ajax({
     success: function(repos) {
         for (var i = 0; i < 6; i++) {
             var project =
-                '<a href="' + repos[i].html_url + '">\
+                '<a href="' + repos[i].html_url + '" target="_blank">\
                     <div class="project" style="background-color:' + repos[i].language + ';">\
                     <br \><br \><br \>\
                         <h1>' + repos[i].name.charAt(0).toUpperCase() + '</h1>\

0afa17d48986f11650c1acd1c859a62748994b1b
Hamdan Javeed <hamdan@hamdanjaveed.com>
2015-04-09 20:35:36 -0400

    Clean up compile.js, fix some bugs

--- compile.js
+++ compile.js
@@ -1,36 +1,41 @@
-var fs = require('fs')
+var fs = require('fs');
 
 var rework = require('rework');
 var myth = require('myth');
 var clean = require('clean-css');
 var inherit = require('rework-inherit');
-var im = require('rework-import');
+var imprt = require('rework-import');
 
-var watch = require('watch')
+var watch = require('watch');
 
-watch.createMonitor('css/stylesheets', function(m) {
+var stylesFilePath = "css/stylesheets";
+var sourceFilePath = stylesFilePath + "/main.css";
+var buildFilePath = "css/build/build.css";
+
+watch.createMonitor(stylesFilePath, function(m) {
     m.on("created", recompile);
     m.on("changed", recompile);
     m.on("removed", recompile);
 });
 
-recompile();
-
 function recompile() {
-    console.log("Compiling...");
-
     try {
-        var rawCSS = fs.readFileSync('css/stylesheets/style.css', 'utf-8');
+        var rawCSS = fs.readFileSync(sourceFilePath, 'utf-8');
+
         var css = rework(rawCSS)
-            .use(im({ path: 'css/stylesheets' }))
-            .use(myth( { compress: true } ))
+            .use(imprt({ path: stylesFilePath }))
+            .use(myth({ compress: true }))
             .toString()
 
         css = new clean().minify(css).styles;
 
-        fs.writeFileSync('css/build/build.css', css);
+        fs.writeFileSync(buildFilePath, css);
+        
+        console.log(new Date() + ": Compiled.");
     } catch(e) {
-        console.log("Could not compile, error present.");
+        console.log(new Date() + ": Could not compile: ", e);
     }
 }
+
+recompile();
 

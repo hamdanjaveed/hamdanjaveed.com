cc4ec1024f2cbc50a022f7e96cdfd8725b4102ac
Hamdan Javeed <hamdan@hamdanjaveed.com>
2015-04-11 19:20:30 -0400

    Separate page loading logic

---
+++ scripts/pageLoader.js
@@ -0,0 +1,45 @@
+var domContent = document.getElementsByTagName('main')[0];
+
+function goToPage(url) {
+    getHTML(url, setMainContent);
+}
+
+function getHTML(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'document';
+    
+    xhr.onload = function(e) {
+        if (xhr.readyState !== 4) return;
+        if (this.status !== 200) return console.log("GET " + url + " failed with status: " + this.status);
+
+        callback(this.response.firstChild);
+    }
+
+    xhr.onerror = function(err) {
+        console.log("Failed to GET " + url, err);
+    }
+
+    xhr.send(null);
+}
+
+function setMainContent(node) {
+    attachStatefulEventListeners(node);
+
+    domContent.innerHTML = '';
+    domContent.appendChild(node);
+}
+
+function attachStatefulEventListeners(node) {
+    var links = node.getElementsByClassName('stateful');
+
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', function(e) {
+            goToPage(e.target.href);
+
+            e.preventDefault();
+        }, false);
+    }
+}
+
+export { goToPage, attachStatefulEventListeners };
